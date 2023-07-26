084c3714254922b57f359a0cd04c5aa1c18818ab
Xinlin Zhang <xz2863@columbia.edu>
2023-07-26 19:39:30 +0800

    client

---
+++ client/src/scences/widgets/AdverWidget.jsx
@@ -0,0 +1,38 @@
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const AdvertWidget = () => {
+  const { palette } = useTheme();
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  return (
+    <WidgetWrapper>
+      <FlexBetween>
+        <Typography color={dark} variant="h5" fontWeight="500">
+          Sponsored
+        </Typography>
+        <Typography color={medium}>Create Ad</Typography>
+      </FlexBetween>
+
+      <img
+        width="100%"
+        height="auto"
+        alt="adver"
+        src="http://localhost:3001/assets/kattie.jpeg"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+      <FlexBetween>
+        <Typography color={main}>Kattie</Typography>
+        <Typography color={medium}>kattiecatmmunity.com</Typography>
+      </FlexBetween>
+      <Typography color={medium} m="0.5rem 0">
+        Bring your kitty to Hang Out!
+      </Typography>
+    </WidgetWrapper>
+  );
+};
+
+export default AdvertWidget;
